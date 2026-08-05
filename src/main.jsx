c4bc9e81e54b9c862c461bebdb77ee1ddc1b109c
ipin <kevinjonatan743@gmail.com>
2026-08-05 23:41:23 +0700

    feat:add pxlkit components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { PxlKitToastProvider } from '@pxlkit/ui-kit'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PxlKitToastProvider position="bottom-right">
     <App />
+    </PxlKitToastProvider>
   </React.StrictMode>,
 )
